ec34e5bae470c9c418f20ca1987c6aee7ca3b981
Amr Mousa <amrmousa1682@gmail.com>
2023-10-07 20:46:36 +0300

    fix script.sql

--- src/main/java/com/example/userapi/script/script.sql
+++ src/main/java/com/example/userapi/script/script.sql
@@ -4,5 +4,6 @@ CREATE TABLE users
     id       INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255)        NOT NULL,
-    role     VARCHAR(255)        NOT NULL
+    role     VARCHAR(255)        NOT NULL,
+    enable   BOOLEAN             NOT NULL
 );
